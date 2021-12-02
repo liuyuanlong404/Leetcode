9aef577dc35db3416e27346af0d818d466ed21b1
Lakers <1501862490@qq.com>
2021-12-02 14:07:44 +0800

    String 前三题

--- src/main/java/com/rose/string/Test2.java
+++ src/main/java/com/rose/string/Test2.java
@@ -1,32 +1,28 @@
 package com.rose.string;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.rose.util.BankCardDetectionUtil;
-import lombok.Data;
-
 /**
  * Created on 2021/11/4 17:53
  *
  * @author Rose
+ *                      整数反转
+ *  给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
+ *
+ * 如果反转后整数超过 32 位的有符号整数的范围[−231, 231− 1] ，就返回 0。
+ *
+ * 作者：力扣 (LeetCode)
+ * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnx13t/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
  */
 public class Test2 {
 
-    @Data
-    public static class Bank{
-        private String cardType;
-        private String bank;
-        private String key;
-        private String[] messages;
-        private Boolean validated;
-        private String stat;
-    }
+    public static int reverse(int x) {
+        long res = 0;
+        while (x != 0){
+            res = res * 10 + x % 10;
+            x = x / 10;
+        }
 
-    public static void main(String[] args) {
-
-        String cardDetail = BankCardDetectionUtil.getCardDetail("6217903600001081560");
-        System.out.println(cardDetail);
-        Bank bank = JSONObject.parseObject(cardDetail, Test2.Bank.class);
-        System.out.println(bank);
+        return (int) res == res ? (int) res : 0;
     }
 }
---
+++ src/main/java/com/rose/string/Test4.java
@@ -0,0 +1,9 @@
+package com.rose.string;
+
+/**
+ * Created on 2021/12/2 14:06
+ *
+ * @author Lakers
+ */
+public class Test4 {
+}
